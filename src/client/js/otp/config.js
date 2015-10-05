a8cb2ad6225547ff3c2a27973e7b5714adca4809
jailby <jbsubils@gmail.com>
2015-10-05 10:20:57 -0400

    Fixed 93. The layers name have been change for a better understanding from the user.

--- src/client/js/otp/config.js
+++ src/client/js/otp/config.js
@@ -44,13 +44,13 @@ otp.config = {
      
     baseLayers: [
         {
-            name: 'MapQuest OSM',
+            name: 'Map',
             tileUrl: 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
             subdomains : ['otile1','otile2','otile3','otile4'],
             attribution : 'Data, imagery and map information provided by <a href="http://open.mapquest.com" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors.'
         },
         {
-            name: 'MapQuest Aerial',
+            name: 'Satellite',
             tileUrl: 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png',
             subdomains : ['otile1','otile2','otile3','otile4'],
             attribution : 'Data, imagery and map information provided by <a href="http://open.mapquest.com" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors.'
