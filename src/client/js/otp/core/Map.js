839bf523396c952d32718410704902ca02d24f57
jmfield2 <jmfield2@mail.usf.edu>
2016-12-15 00:34:03 -0500

    + Add a basic String.prototype.includes function to check for the existence of a substring (using indexOf) on platforms (#386)
    JS cleanup for IE11 and possibly others

--- src/client/js/otp/core/Map.js
+++ src/client/js/otp/core/Map.js
@@ -39,6 +39,11 @@ otp.core.Map = otp.Class({
         //var baseLayers = {};
         var defaultBaseLayer = null;
 
+        // Support older browsers like IE11 which don't include this function
+        if (String.prototype.includes == undefined) {
+               String.prototype.includes = function(x) { return this.indexOf(x) > -1; };
+        }
+
         // http://stackoverflow.com/questions/19689715/what-is-the-best-way-to-detect-retina-support-on-a-device-using-javascript
         function isRetina(){
             return ((window.matchMedia && (window.matchMedia('only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx), only screen and (min-resolution: 75.6dpcm)').matches || window.matchMedia('only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2/1), only screen and (min--moz-device-pixel-ratio: 2), only screen and (min-device-pixel-ratio: 2)').matches)) || (window.devicePixelRatio && window.devicePixelRatio >= 2)) && /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
